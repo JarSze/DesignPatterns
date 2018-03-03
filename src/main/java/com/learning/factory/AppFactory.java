31ef8768b55d74877d06b2d91116b31a5b5a4a8e
jaroslav szeligowski <j.szeligowski@gmail.com>
2018-03-03 15:09:39 +0100

    changed test factory app

--- src/main/java/com/learning/factory/AppFactory.java
+++ src/main/java/com/learning/factory/AppFactory.java
@@ -5,6 +5,8 @@ import com.learning.factory.model.Employee;
 import com.learning.factory.writer.CompanyWriter;
 import com.learning.factory.writer.XmlCompanyWriter;
 
+import java.util.Scanner;
+
 /**
  * Created by jszeligowski on 2018-03-03.
  */
@@ -16,11 +18,26 @@ public class AppFactory
         Employee owner = new Employee( "jan", "nowak", "jnowak@company.com", 1000 );
         Employee director = new Employee( "jan", "kowalski", "jkowalski@company.com", 2000 );
 
-        Company company=new Company();
+        Company company = new Company();
         company.addEmployee( owner );
-        company.addEmployee(director);
+        company.addEmployee( director );
 
-        CompanyWriter writer = new XmlCompanyWriter("firma.xml");
+        CompanyWriter writer = new XmlCompanyWriter( "firma.xml" );
         writer.write( company );
+
+        Scanner scanner = new Scanner( System.in );
+        System.out.println("Enter file name");
+
+        String filename = scanner.next();
+        CompanyWriterFactory companyWriterFactory = new CompanyWriterFactory();
+        try
+        {
+            CompanyWriter writerr = companyWriterFactory.create( filename );
+            writer.write( company );
+        }
+        catch( IllegalArgumentException ex )
+        {
+            System.out.println( ex.getMessage() );
+        }
     }
 }
