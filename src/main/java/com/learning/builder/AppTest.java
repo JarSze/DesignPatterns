bd1b453337e84ea45b0c9bbd00f313ea42816cfe
jaroslav szeligowski <j.szeligowski@gmail.com>
2018-03-03 11:06:44 +0100

    added usage of builer in AppTest class

--- src/main/java/com/learning/builder/AppTest.java
+++ src/main/java/com/learning/builder/AppTest.java
@@ -1,5 +1,7 @@
 package com.learning.builder;
 
+import java.time.LocalDate;
+
 /**
  * Created by jszeligowski on 2018-03-03.
  */
@@ -7,6 +9,26 @@ public class AppTest
 {
     public static void main( String[] args )
     {
+        Reservation.ReservationBuilder reservationBuilder = Reservation.builder()
+            .city( "Poznan" )
+            .headcount( 200 )
+            .price( 100 );
+
+        //read from file
+        reservationBuilder.data( LocalDate.parse( "2018-03-03" ) );
+
+        //end build process - recive object
+        Reservation build =reservationBuilder.build();
+        System.out.println(build);
+
+        //another case - i know all values
+        Reservation wroclaw = Reservation.builder()
+            .data( LocalDate.parse( "2019-04-04" ) )
+            .city( "Wroclaw" )
+            .headcount( 300 )
+            .price( 500 )
+            .build();
+
 
     }
 }
