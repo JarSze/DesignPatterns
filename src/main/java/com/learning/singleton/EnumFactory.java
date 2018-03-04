6c16358353bbe094cd197d6d8ed1c6ae48e23e87
jaroslav szeligowski <j.szeligowski@gmail.com>
2018-03-04 09:32:43 +0100

    changed reflection playgroung for singleton and added enum factory class

---
+++ src/main/java/com/learning/singleton/EnumFactory.java
@@ -0,0 +1,8 @@
+package com.learning.singleton;
+
+/**
+ * Created by jszeligowski on 2018-03-04.
+ */
+public class EnumFactory
+{
+}
--- src/main/java/com/learning/singleton/ReflectionPlayground.java
+++ src/main/java/com/learning/singleton/ReflectionPlayground.java
@@ -2,6 +2,8 @@ package com.learning.singleton;
 
 import com.learning.factory.model.Employee;
 
+import java.lang.reflect.Constructor;
+
 /**
  * Created by jszeligowski on 2018-03-03.
  */
@@ -16,5 +18,22 @@ public class ReflectionPlayground
         if(o instanceof Employee){
             System.out.println("o is an employee object");
         }
+
+        SingletonCompanyWriterFactory instance = SingletonCompanyWriterFactory.getInstance();
+        Constructor<?>[] declaredConstructors = SingletonCompanyWriterFactory.class.getDeclaredConstructors(  );
+        for(Constructor c: declaredConstructors ){
+            c.setAccessible( true );
+        }
+
+        Constructor<?> declaredConstructor = declaredConstructors[0];
+        SingletonCompanyWriterFactory oo = (SingletonCompanyWriterFactory) declaredConstructor.newInstance(  );
+        if (instance==oo){
+            System.out.println("the same object");
+
+        }
+        else{
+            System.out.println("Not the same object");
+        }
+
     }
 }
