0e156c4d8ae7c47043b8334894a1ad2763c65602
jaroslav szeligowski <j.szeligowski@gmail.com>
2018-03-04 10:21:39 +0100

    added decorator called ColoredCircle which decorates Circle

---
+++ src/main/java/com/learning/decorator/shapedecorator/ColoredCircle.java
@@ -0,0 +1,23 @@
+package com.learning.decorator.shapedecorator;
+
+/**
+ * Created by jszeligowski on 2018-03-04.
+ */
+public class ColoredShape implements Shape
+{
+
+    private String color;
+    private Circle circleToDecorate;
+
+
+    public ColoredCircle(Circle circle, String color){
+        this.color=color;
+        this.circleToDecorate=circle;
+    }
+
+
+    @Override
+    public String info(){
+        return circleToDecorate.info()+" has color "+color;
+    }
+}
---
+++ src/main/java/com/learning/decorator/shapedecorator/Shape.java
@@ -0,0 +1,8 @@
+package com.learning.decorator.shapedecorator;
+
+/**
+ * Created by jszeligowski on 2018-03-04.
+ */
+public interface Shape
+{
+}
